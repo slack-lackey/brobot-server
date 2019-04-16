357a5e97fde4290ea43b3d339dc4859886368ad6
Billy Bunn <williammbunn@gmail.com>
2019-04-15 18:44:23 -0700

    refactors save gist functionality

--- index.js
+++ index.js
@@ -141,38 +141,17 @@ slackEvents.on('message', (message, body) => {
   }
 
   if (!message.subtype && message.text.indexOf('save gist') >= 0) {
-    // console.log('save gist message:', message);
+    console.log('save gist message:', message);
         // console.log('save gist message:', message);
     const slack = getClientByTeamId(body.team_id);
 
-    return superagent.post('https://api.github.com/gists')
-      .set('Authorization', `token ${process.env.GIST_TOKEN}`)
-      .send({
-        "description": "Hello World Examples",
-        "public": true,
-        "files": {
-          "hello_world.rb": {
-            "content": "class HelloWorld\n   def initialize(name)\n      @name = name.capitalize\n   end\n   def sayHi\n      puts \"Hello !\"\n   end\nend\n\nhello = HelloWorld.new(\"World\")\nhello.sayHi"
-          },
-          "hello_world.py": {
-            "content": "class HelloWorld:\n\n    def __init__(self, name):\n        self.name = name.capitalize()\n       \n    def sayHi(self):\n        print \"Hello \" + self.name + \"!\"\n\nhello = HelloWorld(\"world\")\nhello.sayHi()"
-          },
-          "hello_world_ruby.txt": {
-            "content": "Run `ruby hello_world.rb` to print Hello World"
-          },
-          "hello_world_python.txt": {
-            "content": "Run `python hello_world.py` to print Hello World"
-          }
-        }
-      })
+    return superagent.post(`${process.env.BOT_API_SERVER}/createGist`)
+      .send(message)
       .then(res => {
-        console.log('response:', JSON.parse(res.text));
-        const data = JSON.parse(res.text);
-        // Send a link pointing to the new gist
-        const url = `https://gist.github.com/${data.owner.login}/${data.id}`;
+        console.log('response body URL:', res);
         slack.chat.postMessage({
           channel: message.channel,
-          text: 'I saved it as a gist for you. You can find it here:\n' + url
+          text: 'I saved it as a gist for you. You can find it here:\n' + res.text
         });
       })
       .catch(err => console.log(err))
